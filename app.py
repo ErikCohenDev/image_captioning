0db58d10255b63333556a68d2435da4395665659
Erik Cohen <ecohen@benchsci.com>
2022-02-21 23:15:07 -0500

    Remove unused imports

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-import os
 import streamlit as st
 from PIL import Image
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import streamlit as st
 import altair as alt
 import json
